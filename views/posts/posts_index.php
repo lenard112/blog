00060d838495a9a79ad4536e022ab10ab690d25e
lenard.latte <lenard.latte@khk.ee>
2014-01-27 15:54:00 +0200

    Add single post viewing functionality and clean borrowed code

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,22 +1,10 @@
 <?foreach( $posts as $post ):?>
-<div class="container">
-    <div class="col-md-12">
-        <h1><?=$post['post_subject']?></h1>
+    <div class="span8">
+        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
         <p><?=$post['post_text']?></p>
         <div>
-            <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
-                <span class="label label-danger">Danger</span></div>
-        </div>
-        <hr>
-        <h1>Revolution has begun!</h1>
-        <p>'I am bound to Tahiti for more men.'
-            'Very good. Let me board you a moment—I come in peace.' With that he leaped from the canoe, swam to the boat; and climbing the gunwale, stood face to face with the captain.
-            'Cross your arms, sir; throw back your head. Now, repeat after me. As soon as Steelkilt leaves me, I swear to beach this boat on yonder island, and remain there six days. If I do not, may lightning strike me!'A pretty scholar,' laughed the Lakeman. 'Adios, Senor!' and leaping into the sea, he swam back to his comrades.</p>
-        <div>
-            <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
-                <span class="label label-danger">Danger</span></div>
+            <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
         </div>
         <hr>
     </div>
-</div>
 <?endforeach?>
