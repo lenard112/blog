5d7471bc73885c658f920b21686965e4ee661bc8
Lennu <lenard.latte@khk.ee>
2014-03-26 15:40:36 +0200

    Fix code layout, fix tags support, fix minor bugs

--- controllers/tags.php
+++ controllers/tags.php
@@ -1,22 +1,17 @@
 <?php
 class tags extends Controller{
 
-	function index(){
-		$this->tags = get_all("SELECT tag_name, COUNT(post_id) AS count
+    function index(){
+        $this->tags = get_all("SELECT tag_name, tag_id, COUNT(post_id) AS count
 		 FROM post_tags NATURAL JOIN tag
 		 GROUP BY tag_id");
-	}
+    }
 
-	function view(){
-		$tag_name = $this->params[0];
-		$tag_id = get_one("SELECT tag_id FROM tag NATURAL JOIN user WHERE tag_name='$tag_name'");
-		$this->posts = get_all("SELECT * FROM post_tags NATURAL JOIN post, user WHERE tag_id='$tag_id'");
-        $post_id = $this->params[0];
-        $this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
-        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
-        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
-        foreach ($_tags as $tag) {
-            $this->tags[$tag['post_id']][] = $tag['tag_name'];
-        }
-	}
+    function view(){
+        $tag_id= $this->params[0];
+        $this->posts = get_all("SELECT post.*, user.username
+		 FROM post_tags NATURAL JOIN post NATURAL JOIN user WHERE tag_id='$tag_id'
+		 ");
+        $this->tag_name = get_one("SELECT tag_name FROM tag WHERE tag_id='$tag_id'");
+    }
 }
--- templates/master_template.php
+++ templates/master_template.php
@@ -1,65 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="shortcut icon" href="<?=ASSETS_URL?>ico/favicon.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="<?=ASSETS_URL?>ico/favicon.png">
 
-	<title><?=PROJECT_NAME?></title>
+    <title><?=PROJECT_NAME?></title>
 
-	<!-- Bootstrap core CSS -->
-	<link href="<?=ASSETS_URL?>components/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="<?=ASSETS_URL?>components/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- Custom styles for this template -->
-	<style>
-		body {
-			min-height: 2000px !important;
-			padding-top: 70px;
-			background: url(<?= ASSETS_URL ?>img/bg.jpg);
-		}
-	</style>
+    <!-- Custom styles for this template -->
+    <style>
+        body {
+            min-height: 2000px !important;
+            padding-top: 70px;
+            background: url(<?=ASSETS_URL?>img/bg.jpg);
+        }
+    </style>
 
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 
 </head>
 
 <body>
-
-<!-- Fixed navbar -->
 <div class="navbar navbar-default navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="<?=BASE_URL?>"><?=PROJECT_NAME?></a>
-		</div>
-		<div class="navbar-collapse collapse">
-			<ul class="nav navbar-nav">
-				<li <?= $controller == 'posts' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>">Posts</a></li>
-				<li <?= $controller == 'tags' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>tags">Tags</a></li>
-        </div><!--/.nav-collapse -->
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?=BASE_URL?>"><?=PROJECT_NAME?></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li <?= $controller == 'posts' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>">Posts</a></li>
+                <li <?= $controller == 'tags' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>tags">Tags</a></li>
+            </ul>
+        </div>
     </div>
 </div>
 
 <div class="container">
-
-	<!-- Main component for a primary marketing message or call to action -->
     <?  require 'views/'. $controller . '/' .  $controller . '_' . $action . '.php'; ?>
 
-</div> <!-- /container -->
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
+</div>
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="<?=ASSETS_URL?>components/jquery/1.10.2/jquery-1.10.2.min.js"></script>
 <script src="<?=ASSETS_URL?>components/bootstrap/3.0.3/js/bootstrap.min.js"></script>
